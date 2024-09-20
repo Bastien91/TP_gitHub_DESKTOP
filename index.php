aed5a365bf4ed6b3e4237c4e4fc2e6d8e6e7388a
Bastien91 <bm91smo@gmail.com>
2024-09-20 11:47:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="styles.css" rel="stylesheet">
+</head>
+<body>
 <h1>TP 1 GitHub</h1>
+
 <p>Bonjour tout le monde</p>
+
 <h2>changement intenssif</h2>
+
+<p>Petit texte qui devrais s'afficher sur la branche</p>
+
+</body>
+</html>
